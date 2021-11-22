790b1375b53bd247666d90cd4f06138c5e8b0aa5
hnavacas <hnavacas.gmail.com>
2021-11-22 07:23:22 -0500

    Activities01

---
+++ app/src/main/java/com/misiontic2022/packagetracking/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.misiontic2022.packagetracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
